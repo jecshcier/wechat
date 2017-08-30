6e8eb855f27d9e441b4e7eafaa5003afa5c2fe7d
jecshcier <alsk123111@icloud.com>
2017-08-30 23:32:53 +0800

    test

--- routes/wechat.js
+++ routes/wechat.js
@@ -1,5 +1,15 @@
 var config = require(process.cwd() + '/config')
 var wechat = require('wechat')
+var List = require('wechat').List;
+List.add('view', [
+  ['回复{a}查看我的性别', function (info, req, res) {
+    res.reply('我是个妹纸哟');
+  }],
+  ['回复{b}查看我的年龄', function (info, req, res) {
+    res.reply('我今年18岁');
+  }],
+  ['回复{c}查看我的性取向', '这样的事情怎么好意思告诉你啦- -']
+]);
 module.exports = wechat(config.wechatConfig, wechat.text(function(message, req, res, next) {
     // 微信输入信息都在req.weixin上
     console.log(res)
