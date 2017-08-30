3bea1f24abf3f70c6d294683d6027a472f679233
jecshcier <alsk123111@icloud.com>
2017-08-30 22:23:22 +0800

    test

--- routes/wechat.js
+++ routes/wechat.js
@@ -24,6 +24,7 @@ module.exports = wechat(config.wechatConfig, wechat.text(function(message, req, 
     } else if (message.Content === "数据测试") {
         res.reply(JSON.stringify(message));
     } else if (message.Content === "菜单测试") {
+        console.log(res)
         res.wait('view');
     } else {
         res.reply("说的什么玩意？");
