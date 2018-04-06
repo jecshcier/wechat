2e04cafc34e062abb0e8e21d00d94e22ed7eb479
jecshcier <alsk123111@icloud.com>
2018-04-06 20:27:06 +0800

    update

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 {
-    serverDomain:"http://cshayne.cn",
-    serverName:"/wechat",
-    wechatConfig:{
+    serverDomain: "http://cshayne.cn",
+    serverName: "/wechat",
+    wechatConfig: {
         token: "jecshcierWeixin",
         appid: "wx0fd1c0b003749f35",
         encodingAESKey: "e2zUHo3OM3R5JUaDTTacfEcssTyhNLr2xcxh5cirUvp",
         checkSignature: true
     },
-    robotConfig:{
-        apiKey:"865977475bb74c6ba759fc02606b9326",
-        userId:"jecshcierWechat666",
-        url:"http://openapi.tuling123.com/openapi/api/v2"
+    robotConfig: {
+        apiKey: "865977475bb74c6ba759fc02606b9326",
+        userId: "jecshcierWechat666",
+        url: "http://openapi.tuling123.com/openapi/api/v2"
     },
-    staticPathName:"/static",
-    sourceRealPath:null,
-    sourcePathName:"/source",
-    keyWords:["c","C","目录","资源","首页","阿C"]
+    staticPathName: "/static",
+    sourceRealPath: null,
+    sourcePathName: "/source",
+    keyWords: ["c", "C", "目录", "资源", "首页", "阿C"]
 }
